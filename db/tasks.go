be4ef145fe88e8cfba094b56cad0829fb2bed030
KhNikh <nipunkhandelwal1999@gmail.com>
2024-01-12 00:33:11 +0530

    added create task function

--- db/tasks.go
+++ db/tasks.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"encoding/binary"
 	"time"
 
 	"github.com/boltdb/bolt"
@@ -24,4 +25,34 @@ func Init(dbPath string) error {
 		_, err := tx.CreateBucketIfNotExists(taskBucket)
 		return err
 	})
+}
+
+func CreateTask(task string) (int, error) {
+	var id int
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		id64, _ := b.NextSequence()
+
+		// Put method takes bytes as input so we have to convert id64 to bytes
+		id = int(id64)
+		key := itob(id)
+		return b.Put(key, []byte(task))
+	})
+	if err != nil {
+		return -1, err
+	} else {
+		return id, nil
+	}
+}
+
+func itob(id int) []byte {
+	//use binary package to convert int to binary
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(id))
+	return b
+
+}
+
+func btoi(b []byte) int {
+	return int(binary.BigEndian.Uint64(b))
 }
